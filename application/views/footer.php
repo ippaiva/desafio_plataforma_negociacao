fe0081dc9f42c3538071d55c331354ae7f0bb21f
Ismael P. Paiva <ismaelpaiva@172.16.1.65>
2017-03-06 13:43:25 -0300

    config_inicial

---
+++ application/views/footer.php
@@ -0,0 +1,28 @@
+</div>
+
+<div class="row" id="footer_admin">
+    <div class="col-md-12">
+
+    </div>
+</div>
+
+<div class="modal fade" id="msg_modal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Fechar</span></button>
+                <h4 class="modal-title" id="title_modal"></h4>
+            </div>
+            <div class="modal-body">
+                <span id="mensagem_modal"></span>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary" data-dismiss="modal">Fechar</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+
+</html>
